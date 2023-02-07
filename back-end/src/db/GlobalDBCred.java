763a56e3a438ada94bf5a651b24ecb5098ad663b
Yingfan Chen <yingfc@uw.edu>
2023-02-06 22:52:55 -0800

    add dynamic read of db key

--- back-end/src/db/GlobalDBCred.java
+++ back-end/src/db/GlobalDBCred.java
@@ -1,10 +1,30 @@
 package db;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public interface GlobalDBCred {
     String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
     String DB_URL = "jdbc:mysql://34.145.35.221/dubmap_backend?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
 
     // Database credential
     String USER = "root";
-    String PASS = "";     // ask @yingfc for password
+    String PASS = getDbKey();     // ask @yingfc for password
+
+    static String getDbKey() {
+        try {
+            File f = new File("back-end/local.properties");
+            Scanner scan = new Scanner(f);
+            if (scan.hasNext()) {
+                String res = scan.next();
+                return res;
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("file not found");
+            e.printStackTrace();
+        }
+        return "";
+    }
 }
+
