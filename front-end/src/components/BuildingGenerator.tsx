9ed874b62db085c562f1caeb2683472d3bf0a009
Yigao LI <yigaol2@uw.edu>
2023-02-09 13:57:38 -0800

    construct seperate building data access

--- front-end/src/components/BuildingGenerator.tsx
+++ front-end/src/components/BuildingGenerator.tsx
@@ -1,36 +1,57 @@
 import React, { Component } from 'react';
+import * as fs from 'fs';
 import './index.css';
 
 interface BuildingGeneratorProps{
   name: string;
+  buildings: {[abbr: string]:string};
 }
 
 interface Building {
   abbr: string;
   full: string;
+  link: string;
 }
 
 interface BuildingGenerator {
   building : Building;
 }
 
+const reservable: {[abbr: string]: string; } =
+{"ALB": "https://cal.lib.uw.edu/spaces?lid=1449&gid=0",
+"ELB":"https://cal.lib.uw.edu/reserve/engineering-group-study",
+"HSA":"https://cal.lib.uw.edu/reserve/hsl-group-study",
+"ODE":"https://cal.lib.uw.edu/spaces?lid=1454&gid=0",
+"PCAR":"https://cal.lib.uw.edu/reserve/foster-group-study",
+"SUZ":"https://cal.lib.uw.edu/spaces?lid=1449&gid=0"}
+
 
 class BuildingGenerator extends Component<BuildingGeneratorProps, {}> {
-  constructor (props: any) { //props should be string, query building info from db
+  constructor (props: BuildingGeneratorProps) { //props should be string, query building info from db
     super(props);
     this.building = this.fetchBuilding(props);
   }
 
   fetchBuilding (props: BuildingGeneratorProps): Building {
-    // fetch data from room -> building db
+    fs.readFileSync('back-end\\src\\db\\building.csv','utf8');
     var building: Building = {} as Building;
+    building.abbr = props.name;
+    building.full = props.buildings[props.name];
+    // fetch data from room -> building db
+    for (var key in reservable) {
+      if (key === props.name) {
+        building.link = reservable[key];
+      }
+    }
+
     // update data from db
     return building;
   }
 
   parseBuilding (props: Building): JSX.Element {
     let building = <div>
-      <p>{props.full}</p>
+      <p>{props.abbr + " : " + props.full}</p>
+      <a href={props.link}>To Reserve</a>
     </div>
     return building;
   }
