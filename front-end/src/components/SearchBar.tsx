2e6a6f3d1e853310a9a5bbcb8f26cdcc56af7817
Yingfan Chen <yingfc@uw.edu>
2023-02-13 15:59:24 -0800

    add classInfo and getBuildingInfoFromClass for textBox functionality

--- front-end/src/components/SearchBar.tsx
+++ front-end/src/components/SearchBar.tsx
@@ -1,5 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import axios from "axios";
+import {BuildingInfo} from "./index";
+
+export interface ClassInfo {
+  major: string;
+  courseNum: number;
+  section: string;
+}
+
+export async function getBuildingInfoFromClass(cls: ClassInfo) {
+  try {
+    const response = await axios.get(process.env.REACT_APP_DUBMAP_SERVER + "class?major" + cls.major + "&coursenum=" + cls.courseNum + "&section=" + cls.section);
+    return response.data.data as BuildingInfo;
+  } catch {
+    alert("Unable to get specific building from course: " + cls.major + cls.courseNum + cls.section);
+    return null;
+  }
+
+}
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
