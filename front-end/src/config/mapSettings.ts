7ee5b1712397db6dd2428c4038d60b3ecd9dcfad
Chutengli <72440194+Chutengli@users.noreply.github.com>
2023-02-13 13:57:40 -0800

    (ft)removed map default markers

--- front-end/src/config/mapSettings.ts
+++ front-end/src/config/mapSettings.ts
@@ -8,7 +8,72 @@ export const center = {
   lng: -122.3032,
 };
 
-export const options = {
+const mapStyle = [
+  {
+    featureType: "administrative",
+    elementType: "geometry",
+    stylers: [
+      {
+        visibility: "off",
+      },
+    ],
+  },
+  {
+    featureType: "administrative.land_parcel",
+    elementType: "labels",
+    stylers: [
+      {
+        visibility: "off",
+      },
+    ],
+  },
+  {
+    featureType: "poi",
+    stylers: [
+      {
+        visibility: "off",
+      },
+    ],
+  },
+  {
+    featureType: "poi",
+    elementType: "labels.text",
+    stylers: [
+      {
+        visibility: "off",
+      },
+    ],
+  },
+  {
+    featureType: "road",
+    elementType: "labels.icon",
+    stylers: [
+      {
+        visibility: "off",
+      },
+    ],
+  },
+  {
+    featureType: "road.local",
+    elementType: "labels",
+    stylers: [
+      {
+        visibility: "off",
+      },
+    ],
+  },
+  {
+    featureType: "transit",
+    stylers: [
+      {
+        visibility: "off",
+      },
+    ],
+  },
+];
+
+export const options: google.maps.MapOptions = {
   disableDefaultUI: true,
   zoomControl: true,
+  styles: mapStyle,
 };
