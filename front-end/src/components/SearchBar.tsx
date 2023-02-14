aa9ec635e7c551ee68749088df557fac9428d8b6
Yigao LI <yigaol2@uw.edu>
2023-02-13 17:29:36 -0800

    SearchBar function initiated

--- front-end/src/components/SearchBar.tsx
+++ front-end/src/components/SearchBar.tsx
@@ -1,12 +1,46 @@
+import axios from 'axios';
+import { BuildingInfo } from '.';
 import React from 'react';
-import ReactDOM from 'react-dom/client';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-  </React.StrictMode>
-);
+const SearchBar: React.FC = () =>{
+  const parseInput = async (input: string) => {
+    let firstDigit = input.search(/\d/).toString();
+    let numId = input.indexOf(firstDigit);
+    let major = input.substring(0, numId-1);
+    let courseNum = input.substring(numId, numId+3);
+    let section = input.substring(numId+4);
+    let res = await getCourseInfo(major, courseNum, section);
+    console.log(res)
+  }
 
-// search the location with course name
+  const input = document.getElementById('input') as HTMLInputElement;
+
+  const button = document.getElementById("search");
+
+  try{
+    button?.addEventListener("click", () => {parseInput(input?.value)})
+  } catch (e) {
+    console.log(e)
+    console.error("Input Error with access to null element")
+  }
+
+  return (
+  <div id="searchBar">
+    <input id="input" type="text" placeholder='CSE 403 A'/>
+    <button id="search">Submit</button>
+  </div>
+  )
+};
+
+async function getCourseInfo(major:string, courseNum:string, section:string): Promise<BuildingInfo | null> {
+  try {
+    const response = await axios.get(process.env.REACT_APP_DUBMAP_SERVER + "class?major"+
+    major+"&coursenum="+courseNum+"&section="+section);
+    return response.data.data as BuildingInfo;
+  } catch {
+    alert("Unable to fetch buildings info");
+    return null;
+  }
+}
+
+export default SearchBar;
