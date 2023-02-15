282a53655b035d1643b41987eeaaa67260c90a59
Chutengli <72440194+Chutengli@users.noreply.github.com>
2023-02-14 17:36:19 -0800

    updated README and test map

--- front-end/src/tests/googleMap.test.ts
+++ front-end/src/tests/googleMap.test.ts
@@ -1,5 +1,38 @@
 import puppeteer from "puppeteer";
+import * as ReactGoogleMapsApi from "@react-google-maps/api";
+import { render } from "@testing-library/react";
+import GoogleMapComponent from "../components/GoogleMap";
 
-test("should first", () => {
-  expect(1).toEqual(1);
+jest.useRealTimers();
+
+let page: any;
+
+const sleep = async (ms: number) => {
+  await new Promise((res) => {
+    setTimeout(res, ms);
+  });
+};
+
+describe.only("Google Map:", () => {
+  beforeAll(async () => {
+    const browser = await puppeteer.launch({
+      headless: true,
+    });
+    page = await browser.newPage();
+
+    await page.goto("http://localhost:3000");
+  }, 3000);
+
+  test("Loading map correctly", async () => {
+    await sleep(1000);
+
+    if (!page) {
+      throw new Error("Error while loading Puppeteer page");
+    }
+
+    jest.spyOn(ReactGoogleMapsApi, "useJsApiLoader").mockReturnValue({
+      isLoaded: true,
+      loadError: undefined,
+    });
+  });
 });
