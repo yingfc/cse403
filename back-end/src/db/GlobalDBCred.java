4a4c8b1080d8acbeec091ec7c9d483793e5bc6e2
Yingfan Chen <66505438+yingfc@users.noreply.github.com>
2023-02-16 17:27:51 -0800

    Redo DB cred retrieve method via .env file and update CI pipeline (#4)
    * use env file for db cred retrieve
    
    * update log in gradle yaml
    
    * rm localproperties for db cred

--- back-end/src/db/GlobalDBCred.java
+++ back-end/src/db/GlobalDBCred.java
@@ -1,30 +1,27 @@
 package db;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
+import io.github.cdimascio.dotenv.Dotenv;
 
 public interface GlobalDBCred {
     String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
-    String DB_URL = "jdbc:mysql://34.145.35.221/dubmap_backend?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
-
+    String DB_URL = getDbUrl();
     // Database credential
-    String USER = "root";
+    String USER = getDbUser();
     String PASS = getDbKey();     // ask @yingfc for password
 
+    static String getDbUrl() {
+        Dotenv dotenv = Dotenv.configure().load();
+        return dotenv.get("DB_URL");
+    }
+
     static String getDbKey() {
-        try {
-            File f = new File("back-end/local.properties");
-            Scanner scan = new Scanner(f);
-            if (scan.hasNext()) {
-                String res = scan.next();
-                return res;
-            }
-        } catch (FileNotFoundException e) {
-            System.out.println("file not found");
-            e.printStackTrace();
-        }
-        return "";
+        Dotenv dotenv = Dotenv.configure().load();
+        return dotenv.get("DB_PASSWORD");
+    }
+
+    static String getDbUser() {
+        Dotenv dotenv = Dotenv.configure().load();
+        return dotenv.get("DB_USER");
     }
 }
 
